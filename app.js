cdd2ef6142231b117f785c028e5ff039c793d122
Erin Francis <ezzzaf@gmail.com>
2014-05-23 18:26:53 +1000

    Fire the right relay when the open function called

--- app.js
+++ app.js
@@ -36,20 +36,20 @@ app.get("/api/garage/open", function(req, res) {
 		async.series([
 			function(callback) {
 				// Open pin for output
-				gpio.open(config.LEFT_GARAGE_PIN, "output", callback);
+				gpio.open(config.RIGHT_GARAGE_PIN, "output", callback);
 			},
 			function(callback) {
 				// Turn the relay on
-				gpio.write(config.LEFT_GARAGE_PIN, config.RELAY_ON, callback);
+				gpio.write(config.RIGHT_GARAGE_PIN, config.RELAY_ON, callback);
 			},
 			function(callback) {
 				// Turn the relay off after delay to simulate button press
-				delayPinWrite(config.LEFT_GARAGE_PIN, config.RELAY_OFF, callback);
+				delayPinWrite(config.RIGHT_GARAGE_PIN, config.RELAY_OFF, callback);
 			},
 			function(err, results) {
 				setTimeout(function() {
 					// Close pin from further writing
-					gpio.close(config.LEFT_GARAGE_PIN);
+					gpio.close(config.RIGHT_GARAGE_PIN);
 					// Return json
 					res.json("ok");
 				}, config.RELAY_TIMEOUT);
