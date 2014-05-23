4f2236dd3adc2752213c8cb926c5b9d9f8970b5d
Erin Francis <ezzzaf@gmail.com>
2014-05-23 14:16:19 +1000

    Add a password protected 'open' endpoint

--- app.js
+++ app.js
@@ -30,6 +30,36 @@ app.get("/api/ping", function(req, res) {
 	res.json("pong");
 });
 
+app.get("/api/garage/open", function(req, res) {
+	if (req.param('password') == process.env.PASSWORD) {
+		res.json('ok');
+	} else  {
+		res.json('incorrect password :O');
+	}
+	/*async.series([
+		function(callback) {
+			// Open pin for output
+			gpio.open(config.LEFT_GARAGE_PIN, "output", callback);
+		},
+		function(callback) {
+			// Turn the relay on
+			gpio.write(config.LEFT_GARAGE_PIN, config.RELAY_ON, callback);
+		},
+		function(callback) {
+			// Turn the relay off after delay to simulate button press
+			delayPinWrite(config.LEFT_GARAGE_PIN, config.RELAY_OFF, callback);
+		},
+		function(err, results) {
+			setTimeout(function() {
+				// Close pin from further writing
+				gpio.close(config.LEFT_GARAGE_PIN);
+				// Return json
+				res.json("ok");
+			}, config.RELAY_TIMEOUT);
+		}
+	]);*/
+});
+
 app.post("/api/garage/left", function(req, res) {
 	async.series([
 		function(callback) {
